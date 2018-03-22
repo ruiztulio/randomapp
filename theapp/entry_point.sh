4ea93e38cea393378eb0d9945abb5b9a9e12987d
Tulio Ruiz <tulio@vauxoo.com>
2018-03-21 19:49:18 -0600

    [FIX] Use the proper worker count for 1 core (2*n_cores+1) and serve the static files

--- theapp/entry_point.sh
+++ theapp/entry_point.sh
@@ -4,5 +4,5 @@ export DJANGO_SETTINGS_MODULE=randomapp.settings
 exec gunicorn randomapp.wsgi:application \
     --name randomapp_main \
     --bind 0.0.0.0:8000 \
-    --workers 5 \
+    --workers 3 \
     --log-level=info
--- theapp/randomapp/randomapp/wsgi.py
+++ theapp/randomapp/randomapp/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "randomapp.settings")
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+#application = get_wsgi_application()
